993c6b97f81c094109b1244729183564ab625414
ilango451646 <ilango451646@gmail.com>
2022-04-06 10:50:20 +0530

    first push

--- src/test/java/org/sample/Swigg.java
+++ src/test/java/org/sample/Swigg.java
@@ -7,15 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Swigg {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\HP PC\\eclipse-workspace\\LaunchUrl\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\HP PC\\eclipse-workspace\\LaunchUrl\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
-		driver.get(" http://gmail.com/");
-		WebElement userid = driver.findElement(By.id("identifierId"));
-		userid.sendKeys("ilango451646@gmail.com");
-		WebElement userpass = driver.findElement(By.xpath("//span[@class='VfPpkd-vQzf8d']"));
-		userpass.click();
+		WebElement userid = driver.findElement(By.id("email"));
+		userid.sendKeys("ilango");
+		WebElement pass = driver.findElement(By.id("pass"));
+		pass.sendKeys("45676");
 
 	}
 }
